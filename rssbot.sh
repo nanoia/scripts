25efc582083e1f47ac391ec28ab45c1d88ab0771
nanoia <alexandery886@gmail.com>
2018-12-09 13:33:42 +0800

    Update rssbot.sh

--- rssbot.sh
+++ rssbot.sh
@@ -13,14 +13,14 @@ apt install bsdtar -y
 tag=$(wget -qO- https://api.github.com/repos/iovxw/rssbot/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
 cd /root && wget -qO- "https://github.com/iovxw/rssbot/releases/download/${tag}/rssbot-${tag}-linux.zip" | bsdtar -xvf-
-chmod +x /root/rssbot
+chmod +x /root/rssbot/rssbot
 
 read -p "Please paste telegram bot token for rssbot here: " token
 [ -z "${token}" ]
 
 cat > /lib/systemd/system/rssbot.service<<-EOF
 [Service]
-ExecStart=/root/rssbot DATAFILE ${token}
+ExecStart=/root/rssbot/rssbot DATAFILE ${token}
 EOF
 
 systemctl daemon-reload
